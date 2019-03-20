41204e2e32a21be1de3c189d30a71ef32e0f3b0b
Santosh Lokhande <santoshlokhande1@gmail.com>
2019-03-21 01:32:30 +0530

    Properties Update
    Properties Update

--- src/main/java/com/sample/postgress/mapper/EmployeeRowMapper.java
+++ src/main/java/com/sample/postgress/mapper/EmployeeRowMapper.java
@@ -12,9 +12,9 @@ public class EmployeeRowMapper implements RowMapper<Employee> {
     @Override
     public Employee mapRow(ResultSet rs, int arg1) throws SQLException {
         Employee emp = new Employee();
-        emp.setEmployeeId(rs.getString("employeeId"));
-        emp.setEmployeeName(rs.getString("employeeName"));
-        emp.setEmployeeOrg(rs.getString("employeeOrg"));
+        emp.setEmployeeId(rs.getString("EmpID"));
+        emp.setEmployeeName(rs.getString("EmpName"));
+        emp.setEmployeeOrg(rs.getString("Org"));
 
         return emp;
     }
